8210b8354cd69c95471dd78951590c4bb78f9b7f
ofHillsAndLakes <conorohaleck@gmail.com>
2015-05-31 14:07:32 -0700

    Update and rename NEW Primary to Game.java

---
+++ Game.java
@@ -0,0 +1,125 @@
+import java.awt.EventQueue;
+import java.util.ArrayList;
+
+
+public class Game {
+
+	public static void main(String[] args) {
+		
+		//All this does is call the GUI, I kept in code from the windowbuilder, just moved it here.
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIBlackJack window = new GUIBlackJack();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	public static void initialRound(Player p1, GUIBlackJack window){
+		Deck deck = new Deck();
+		Hand pHand = new Hand(); //Player's Hand
+		Hand dHand = new Hand(); //Dealer's Hand
+		
+		deck.shuffle();
+		
+		window.writeToConsole("The game is afoot!");
+		
+		dHand.addCard(deck.dealCard());
+		window.addHandCards(dHand.getHand(), false);
+		window.writeToConsole("" + dHand.getVal());
+		pHand.addCard(deck.dealCard());
+		window.addHandCards(pHand.getHand(), true);
+		window.writeToConsole("" + pHand.getVal());
+		
+		window.writeToConsole("What are you going to do now?");
+		window.waitForInput(p1, deck, pHand, dHand);
+		
+	}
+	
+	
+	public static void nextRound(Player p1, GUIBlackJack window, Deck deck, Hand pHand, Hand dHand, boolean isHit){
+		
+		if(isHit){
+			pHand.addCard(deck.dealCard());
+		}
+		
+		if(dHand.getVal() < 10){
+			dHand.addCard(deck.dealCard());
+		}
+		
+		window.addHandCards(dHand.getHand(), false);
+		window.writeToConsole("" + dHand.getVal());
+		
+		window.addHandCards(pHand.getHand(), true);
+		window.writeToConsole("" + pHand.getVal());
+		
+		if(victoryCheck(pHand, dHand, isHit)){
+			window.writeToConsole("You won!");
+			p1.gamesPlayed += 1;
+			p1.gamesWon += 1;
+			window.gameOff();
+		}
+		else if(defeatCheck(pHand, dHand, isHit)){
+			window.writeToConsole("You lost!");
+			p1.gamesPlayed += 1;
+			window.gameOff();
+		}
+		else{
+			window.writeToConsole("What are you going to do now?");
+			window.waitForInput(p1, deck, pHand, dHand);
+		}
+		
+		
+		
+	}
+	
+	public static boolean victoryCheck(Hand pHand, Hand dHand, boolean hitState){
+		if(hitState){
+			if(dHand.getVal() > 21){
+				defaultVictory();
+				return true;
+			}
+			return false;
+		}
+			if(pHand.getVal() > dHand.getVal()){
+				higherVictory();
+				return true;
+			}
+		return false;
+		
+	}
+	
+	public static boolean defeatCheck(Hand pHand, Hand dHand, boolean hitState){
+		if(hitState){
+			if(pHand.getVal() > 21){
+			return true;
+			}
+			return false;
+		}
+		if(dHand.getVal() > pHand.getVal()){
+			return true;
+		}
+		return false;
+	}
+	
+	public static void defaultVictory(){
+		System.out.println("The dealer went over 21! YOU WON!");
+	}
+	
+	public static void higherVictory(){
+		System.out.println("You got a higher total than the dealer! YOU WON!");
+	}
+	
+	public static void defaultDefeat(){
+		System.out.println("You went over 21! YOU LOST!");
+	}
+	
+	public static void higherDefeat(){
+		System.out.println("The dealer got a higher total than you! YOU LOST!");
+	}
+
+}
