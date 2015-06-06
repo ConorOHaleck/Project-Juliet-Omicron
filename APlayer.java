e57f08c24f3c784da8546e3abfe5171ae94f9e90
RobertBohlman <robertbohlman@hotmail.com>
2015-06-06 15:29:10 -0700

    Update APlayer.java

--- APlayer.java
+++ APlayer.java
@@ -3,21 +3,28 @@ import javax.swing.JOptionPane;
 public class Player {
 	
 	String name;
-	int cash;
+	public static double cash;
 	int gamesPlayed;
 	int gamesWon;
 	
 	
 	Player(){
+		try{
 		name = JOptionPane.showInputDialog("What, good sir, is your name?");
+		String c = JOptionPane.showInputDialog("How much would you like to set aside for betting?");
+		cash = Double.parseDouble(c);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Goodbye!");
+			System.exit(1);
+		}
 	}
 	
-	Player(String nomenclature){
+	/*Player(String nomenclature){
 		name = nomenclature;
 	}
 	
 	Player (String nomenclature, int cashola){
 		name = nomenclature;
 		cash = cashola;
-	}
+	}*/
 }
